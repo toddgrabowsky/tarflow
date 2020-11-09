338a4e56471ad584f2b4c6022e37751cb85de970
Todd Grabowsky <todd.grabowsky@trituraig.com>
2020-11-09 16:40:28 +0000

    added some documentation to indicate that the path in the "use_tarflow" function is relative to the RPproj home direectory.

--- R/use_tarflow.R
+++ R/use_tarflow.R
@@ -5,11 +5,11 @@
 ##' @export
 ##' @title use_tarflow
 ##' @return Nothing. Modifies your workspace.
-##' @param path where to initialize the tarflow project
+##' @param path where to initialize the tarflow project (NOTE: this will be relative to your Rproj home directory)
 ##' @param data whether or not to include a 'data' directory
 use_tarflow <- function(path = ".", data = TRUE) {
-  usethis::use_directory(file.path(path ,"R"))
+  usethis::use_directory(file.path(path, "R"))
   if (data) usethis::use_directory(file.path(path, "data"))
   usethis::use_template("_targets.R", save_as = file.path(path, "_targets.R"), package = "tarflow")
-  usethis::use_template("functions.R", save_as = file.path(path, "R", "functions.R"), package = "tarflow")
+  usethis::use_template("functions.R", save_as = file.path(path,"R", "functions.R"), package = "tarflow")
 }
